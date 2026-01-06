1889fb6b78afbf1d7a8afd34dc8fa780ba9dd9c1
Git Bot 1010 <188122220+colorfulLight48@users.noreply.github.com>
2026-01-06 15:35:29 -0500

    Release 1.0.11: Put the output of the start lib into the returned statement instead of standard out in preperation for 1.1.0

--- src/command_start.rs
+++ src/command_start.rs
@@ -4,5 +4,12 @@ pub fn start(_start_matches: &clap::ArgMatches) {
     let name = _start_matches
             .get_one::<String>("name")
             .expect("required argument");
-    crate::command_start_lib::start(name);
+    let output = crate::command_start_lib::start(name).unwrap_or_else(|e| {
+        eprintln!("Error listing timers: {}", e);
+        String::new()  // Returning an empty string on error
+    });
+
+    if !output.is_empty() {
+        println!("{}", output);  // Print the output
+    }
 }
